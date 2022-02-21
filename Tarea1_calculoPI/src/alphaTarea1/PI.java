1a869e5e046a717ce298102332d4e0913aedf5f8
N-Lagunes <73557304+N-Lagunes@users.noreply.github.com>
2022-02-20 20:09:23 -0600

    Version funcional
    Esta version es completa y funcional, no posee comentarios.

--- Tarea1_calculoPI/src/alphaTarea1/PI.java
+++ Tarea1_calculoPI/src/alphaTarea1/PI.java
@@ -9,10 +9,10 @@ import java.net.Socket;
 
 public class PI {
     static double PI;
-    static Object obj = new Object();
-    static class Worker extends Thread{
-        int nodo;
+    static final Object obj = new Object();
 
+    static class Worker extends Thread{
+    int nodo;
         Worker(int nodo){
             this.nodo = nodo;
         }
@@ -21,44 +21,61 @@ public class PI {
         public void run() {
             try {
                 Socket conexion = null;
+                int puerto = 50000+nodo, cont = 0;
+
                     // Intento de reconexión
                     for (;;){
                         try {
-                            conexion = new Socket("localhost", 50000+nodo);
+                            conexion = new Socket("localhost", puerto);
                             break;
-                        }catch(Exception ex){Thread.sleep(3000);}
-                    System.out.println("Conectado al Nodo: "+nodo);
-                    DataInputStream DIS = new DataInputStream(conexion.getInputStream());
-                    double suma = DIS.readDouble();
+                        }catch(Exception ex){Thread.sleep(100);}
+                    }
+                    System.out.println("Conectado al Nodo: "+ nodo +" en el puerto: "+ puerto);
 
+                    DataInputStream entrada = new DataInputStream(conexion.getInputStream());
+
+                    double suma = entrada.readDouble();
+                    System.out.println("Valor de PI en el cliente: "+puerto+" suma: "+suma);
                     synchronized(obj){
                         PI += suma;// debemos ocupar synchronized
                     }
-                    DIS.close();
+
+                    entrada.close();
                     conexion.close();
-                }}catch(Exception ex){ ex.printStackTrace();}
+
+                }catch(Exception ex){ ex.printStackTrace();}
         }
 
     }
+ //https://www.java67.com/2020/05/how-to-deal-with-javanetsocketexception-connection-reset-client-server-error.html
 
     static void servidor(int nodo) throws IOException {
-        ServerSocket servidor = new ServerSocket(50000+nodo);
-        System.out.println("Esperando conmexion en el nodo: "+ nodo);
+        int puerto = 50000+nodo; ServerSocket servidor = new ServerSocket(puerto);
+
+        System.out.println("Esperando conexion en el nodo: "+ nodo +" en el puerto: "+ puerto);
+
         Socket conexion = servidor.accept();
+
+        System.out.println("Conexion exitosa en el puerto: "+ puerto);
 
         DataOutputStream salida = new DataOutputStream(conexion.getOutputStream());
         DataInputStream entrada = new DataInputStream(conexion.getInputStream());
 
-        double suma = entrada.readDouble();
+        double suma=0.0;
+
         for ( int i=0; i<1000000; i++ ) {
             suma += 4.0/(8*i+2*(nodo-2)+3);
         }
         suma = (nodo%2 == 0) ? (-suma) : (suma);
+
+        System.out.println("Valor de PI: "+PI+"En el servior con el puerto"+puerto);
         salida.writeDouble(suma);
 
         entrada.close();
         salida.close();
         conexion.close();
+        servidor.close();
+
     }
 
     public static void main(String[] args) throws InterruptedException, IOException {
@@ -67,7 +84,7 @@ public class PI {
         if ( args.length == 1 ) {
 
             if(nodo == 0 ){
-                Worker cliente[] = new Worker[4];
+                Worker[] cliente = new Worker[4];
                 for (int i = 0; i <4 ; i++){
                     cliente[i] = new Worker(i+1);
                     cliente[i].start();
@@ -78,17 +95,8 @@ public class PI {
                 }
                 System.out.println("Valor de pi: "+PI);
 
-            }else if (nodo <=4 ){
-                switch(nodo){
-                    case 1: servidor(nodo);
-                        break;
-                    case 2: servidor(nodo);
-                        break;
-                    case 3: servidor(nodo);
-                        break;
-                    case 4: servidor(nodo);
-                        break;
-                }
+            }else if (nodo > 0 && nodo <=4 ){
+                servidor(nodo);
             }
         }else{
             System.exit(0);
